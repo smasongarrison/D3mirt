6eeb2c842812f2fc9546c2c45bb7ee1f318db45a
ForsbergPyschometrics <erik.forsberg@psychology.su.se>
2023-03-08 17:58:27 +0100

    Update dmirt.R

--- R/dmirt.R
+++ R/dmirt.R
@@ -1,4 +1,40 @@
+#' dmirt: Estimation of D3mirt R3 Object
+#'
+#' @description The `dmirt`function creates an R3 object that contains all necessary estimates for the three dimensional measurement model. Input consists of a data frame that holds factor loadings and difficulty parameters for all items in the scale or set. Importantly, rows must be items and number of columns must be ≥ 4, i.e., three columns for factor loadings and at least one column for item difficulty. The vector arrows that can be plotted with the `plot()` function represents item characteristics the three dimensional theta space. More specifically, item location shows the items three dimensional level of difficulty. If Likert items are used, each item will have multiple level of difficulty and can therefore be said to show the difficulty range of an item. Moreover, the angle of the vector arrows shows the direction of optimal discrimination in the model. In turn, this indicates what traits, one up to three, the item can be said to describe. Lastly, the length of each vector arrows shows the strength of discrimination, in which longer arrows indicate higher discrimination. Short vector arrows are therefore signs of model violations. The user has the option of including constructs in the estimation, by creating one or more nested lists that states what items belongs to what construct (see the examples section). From this, the `dmirt`function extract direction vectors by averaging the direction cosines for the items contained in each construct list. The constructs vector arrows, therefore, can contribute by visualizing the average direction for at larger set of items. However, the lenght of the construct arrow has no meaning and can be set by the user by changing the `max.norm` and `min.norm` arguments.
+#' @param x data frame or matrix
+#' @param constructs Nested list or lists with integers indicating what items are part of what construct. Default is NULL.
+#' @param max.norm Scaling factor for the length of the construct arrow above center in the model. Default is set to 1.1.
+#' @param min.norm Scaling factor for the length of the construct arrow below center in the model. Default is set to 0.6.
+#'
+#' @return R3 object that contain lists with estimates.
+#' @export
+#'
+#' @examples
+#' # Fitting three dimensional mirt S4 object with the graded response model as item model
+#' library(mirt)
+#' spec <- ' F1 = 1-10
+#'           F2 = 1-10
+#'           F3 = 1-10
+#'
+#'           START=(I_05,a2,0)
+#'           START=(I_05,a3,0)
+#'
+#'           START=(I_30,a3,0)
+#'
+#'           FIXED=(I_05,a2)
+#'           FIXED=(I_05,a3)
+#'
+#'           FIXED=(I_30,a3) '
+#'
+#'
+#' mod1 <- mirt(x, spec, itemtype='graded', SE=T, method = 'QMCEM')
+#' d <- data.frame(mirt::coef(mod.1, simplify=T)$'items'[,1:7])
+#'
+#' # The example below use constructs in the `dmirt´estimation
+#' c <- list(list(1,3,4,6,8), list(2,5,7,9,10))
+#' g <- dmirt(d, c)
 dmirt <- function(x, constructs = NULL, max.norm = 1.1, min.norm = 0.6){
+  # Warning for format
   if(!is.data.frame(x) && !is.matrix(x)) stop("Input object is not of type data frame or matrix")
   if(ncol(x) < 4) stop("Data frame must have at least 4 columns")
   x <- as.matrix(x, drop = FALSE)
@@ -39,8 +75,8 @@ dmirt <- function(x, constructs = NULL, max.norm = 1.1, min.norm = 0.6){
       }
       cscos <- matrix(colSums(cos), ncol = 3)
       cdcos <- 1/sqrt(rowSums(cscos^2))*cscos
-      maxnorm <- (1.1*max(vector))*cdcos
-      minnorm <- (0.6*min(vector))*cdcos
+      maxnorm <- (max.norm*max(vector))*cdcos
+      minnorm <- (min.norm*min(vector))*cdcos
       con <- matrix(rbind(con,rbind(minnorm, maxnorm)), ncol = 3)
       ncos <- matrix(rbind(ncos,cdcos), ncol = 3)
       cdeg <- matrix(rbind(cdeg,(acos(cdcos)*(180/pi))), ncol = 3)
@@ -54,7 +90,7 @@ dmirt <- function(x, constructs = NULL, max.norm = 1.1, min.norm = 0.6){
   dcos <- as.data.frame(dcos)
   colnames(dcos) <- c("D.Cos X", "D.Cos Y", "D.Cos Z")
   deg <- as.data.frame(deg, drop = FALSE)
-  colnames(deg) <- c("X.Deg", "Y.Deg", "Z.Deg")
+  colnames(deg) <- c("X°", "Y°", "Z°")
   mdiff <- as.data.frame(mdiff, drop = FALSE)
   sapply(ncol(mdiff), function(x){
     colnames(mdiff) <- paste("d", 1:x, sep = "")})
@@ -67,7 +103,7 @@ dmirt <- function(x, constructs = NULL, max.norm = 1.1, min.norm = 0.6){
     ncos <- as.data.frame(ncos, drop = FALSE)
     colnames(ncos) <- c("D.Cos X","D.Cos Y", "D.Cos Z")
     cdeg <- as.data.frame(cdeg, drop = FALSE)
-    colnames(cdeg) <- c("X.Deg", "Y.Deg", "Z.Deg")
+    colnames(cdeg) <- c("X°", "Y°", "Z°")
     dmirt <- list(loadings = a, mdisc = mdisc, dir.cos = dcos, degrees = deg, mdiff = mdiff,
                   dir.vec = dir.vec, c = constructs, c.dir.cos = ncos ,c.degrees = cdeg, c.vec = con)
   } else {
