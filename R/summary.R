b0068a947dd2db8de71411a076a5d547a4b3da68
ForsbergPyschometrics <erik.forsberg@psychology.su.se>
2023-03-14 18:08:00 +0100

    Update summary.R

--- R/summary.R
+++ R/summary.R
@@ -1,21 +1,22 @@
 #' summary.dmirt
 #' @description The `summary.dmirt()` function presents a compressed output from the `dmirt()` S3 object.
 #' @param x `dmirt()` S3 object
-#' @param digits number of digits to show in the output.
 #'
 #' @return Lists # double check
 #' @export
 #'
-#' @details The user have option to reduce the number of digits in the output by varying the digits argument. The output presents one frame with item factor loadings, difficulty parameters the MDISC estimate for each item. This is followed by a frame with columns for the direction cosines and columns for the direction cosines transformed to degrees. If the S3 object contain constructs, an extra frame containing direction cosines and angles in degrees for the constructs will be included in the output.
+#' @details The output presents one frame with item factor loadings, difficulty parameters the MDISC estimate for each item. This is followed by a frame with columns for the direction cosines and columns for the direction cosines transformed to degrees. If the S3 object contain constructs, an extra frame containing direction cosines and angles in degrees for the constructs will be included in the output.
 #'
 #' @examples
+#' \dontrun{
 #' # Preparation
 #' #' c <- list(list(1,3,4,6,8), list(2,5,7,9,10))
 #' g <- dmirt(d, c)
 #'
 #' # Call summary
 #' summary(g)
-summary.dmirt <- function(x, digits = 4){
+#' }
+summary.dmirt <- function(x){
   tab1 <- as.data.frame(x$loadings, drop = FALSE)
   tab2 <- as.data.frame(x$mdiff, drop = FALSE)
   tab3 <- as.data.frame(x$mdisc, drop = FALSE)
@@ -36,9 +37,9 @@ summary.dmirt <- function(x, digits = 4){
     }
   }
   if (!is.null(x$c.dir.cos)){
-    sum <- list(dmirt.est = round(tab1,digits), dmirt.angles = round(tab4, digits), construct.angles = round(tab5, digits))
+    sum <- list(dmirt.est = round(tab1,4), dmirt.angles = round(tab4, 4), construct.angles = round(tab5, 4))
   } else{
-    sum <- list(dmirt.est = round(tab1,digits), dmirt.angles = round(tab4, digits))
+    sum <- list(dmirt.est = round(tab1,4), dmirt.angles = round(tab4, 4))
   }
   if (!is.null(x$c.dir.cos)){
     cat(paste("\n3Dmirt object with", nrow(tab1), "items and", ncol(tab2), "levels of difficulty\n\n"))
