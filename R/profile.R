539cf0e16ca4f030609310e253b2101b71db402e
ForsbergPyschometrics <erik.forsberg@psychology.su.se>
2023-03-14 18:08:35 +0100

    Update profile.R

--- R/profile.R
+++ R/profile.R
@@ -1,5 +1,5 @@
 #' profile: Selection of Respondents for Plotting
-#' @description `profile()` is a wrapper function to `fscores()` from the [mirt] package.
+#' @description `profile()` is a wrapper function to `[mirt::fscores()]` from the `mirt` package that use using `[stats::quantile()]` to select respondent rows. The returned obejct can then be used to plot respondents based on factor scores in the `[D3mirt::plot()]`function.
 #' @param x S4 mirt object.
 #' @param y S4 mirt object or data frame with respondents scores.
 #' @param column Select what column in the input for y to use for selection of respondent rows. Default is 1.
@@ -11,11 +11,13 @@
 #' @param QMC Integration method for extracting respondents factor scores from the `fscores()` function. Default is "QMC".
 #'
 #' @return List of factor scores from the `mirt()` S4 object.
+#' @importFrom stats cov
 #' @export
 #'
 #' @details Respondents factor scores are selected row wise based on the logical condition set by user. Factor scores can then be used to plot respondents as spheres in the 3D `dmirt()` object. Please consult documentation on [mirt::fscores()] for more details and available options regarding the `fscores()` function.
 #'
 #' @examples
+#' \dontrun{
 #' # x is the fitted three dimensional S4 mirt object
 #' # y is a data frame containing scores from the same respondents
 #' p <- profile(x, y, column = 20, condition = ">", prob = .75)
@@ -23,43 +25,44 @@
 #'
 #' # To plot with the D3mirt plot function while hiding item vectors
 #' plot(x, profiles = p, hide = TRUE)
+#' }
 profile <- function(x, y = NULL, column = 1, condition = c(">"), prob = .75, method = "EAP", full.scores = TRUE, full.scores.SE = FALSE, QMC = TRUE){
   if(!isS4(x)) warning("x must be S4 mirt object from mirt package")
-  m <- as.data.frame(fscores(x, method = method, full.scores= full.scores, full.scores.SE = full.scores.SE, QMC=QMC), drop = FALSE)
+  m <- as.data.frame(mirt::fscores(x, method = method, full.scores= full.scores, full.scores.SE = full.scores.SE, QMC=QMC), drop = FALSE)
   if (!is.null(y)){
     if (isS4(y)){
       if(column > 3) warning("Column argument set to high, must be 3 or less for S4 objects")
-      y <- as.data.frame(fscores(y, method = method, full.scores= full.scores, full.scores.SE = full.scores.SE, QMC=QMC), drop = FALSE)
+      y <- as.data.frame(mirt::fscores(y, method = method, full.scores= full.scores, full.scores.SE = full.scores.SE, QMC=QMC), drop = FALSE)
       y <- as.matrix(y[,column, drop = FALSE])
     } else {
       if(!is.data.frame(y) && !is.matrix(y)) stop("Input object is not of type data frame or matrix")
       y <- as.matrix(y[,column, drop = FALSE])
     }
     if (condition == ">"){
-      m$dummy <- ifelse(y[,1] > quantile(y, prob = prob),1,0)
+      m$dummy <- ifelse(y[,1] > stats::quantile(y, prob = prob),1,0)
     }
     else if (condition == "<"){
-      m$dummy <- ifelse(y[,1] < quantile(y, prob = prob),1,0)
+      m$dummy <- ifelse(y[,1] < stats::quantile(y, prob = prob),1,0)
     }
     else if (condition == ">="){
-      m$dummy <- ifelse(y[,1] >= quantile(y, prob = prob),1,0)
+      m$dummy <- ifelse(y[,1] >= stats::quantile(y, prob = prob),1,0)
     }
     else if (condition == "<="){
-      m$dummy <- ifelse(y[,1] <= quantile(y, prob = prob),1,0)
+      m$dummy <- ifelse(y[,1] <= stats::quantile(y, prob = prob),1,0)
     }
   }
   if (is.null(y)){
     if (condition == ">"){
-      m$dummy <- ifelse(m[,column[1]] > quantile(m[,column], prob = prob),1,0)
+      m$dummy <- ifelse(m[,column[1]] > stats::quantile(m[,column], prob = prob),1,0)
     }
     else if (condition == "<"){
-      m$dummy <- ifelse(m[,column[1]] < quantile(m[,column], prob = prob),1,0)
+      m$dummy <- ifelse(m[,column[1]] < stats::quantile(m[,column], prob = prob),1,0)
     }
     else  if (condition == ">="){
-      m$dummy <- ifelse(m[,column[1]] >= quantile(m[,column], prob = prob),1,0)
+      m$dummy <- ifelse(m[,column[1]] >= stats::quantile(m[,column], prob = prob),1,0)
     }
     else  if (condition == "<="){
-      m$dummy <- ifelse(m[,column[1]] <= quantile(m[,column], prob = prob),1,0)
+      m$dummy <- ifelse(m[,column[1]] <= stats::quantile(m[,column], prob = prob),1,0)
     }
   }
   m[m$dummy == 1, ]
