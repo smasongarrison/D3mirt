4d1a76c5ca76ea2bf9f8cbf2db1e6384a6c46015
ForsbergPyschometrics <erik.forsberg@psychology.su.se>
2023-03-16 17:16:43 +0100

    Update summary.R

--- R/summary.R
+++ R/summary.R
@@ -1,6 +1,8 @@
 #' summary.dmirt
 #' @description The `summary.dmirt()` function presents a compressed output from the `dmirt()` S3 object.
-#' @param x `dmirt()` S3 object
+#' @param object S3 object of class `D3mirt`.
+#' @param ... Additional arguments to be passed to methods.
+#' @param digits User can adjust number of digiots shown per estimate. Default is `digits = 4`.
 #'
 #' @return Lists # double check
 #' @export
@@ -16,15 +18,15 @@
 #' # Call summary
 #' summary(g)
 #' }
-summary.dmirt <- function(x){
-  tab1 <- as.data.frame(x$loadings, drop = FALSE)
-  tab2 <- as.data.frame(x$mdiff, drop = FALSE)
-  tab3 <- as.data.frame(x$mdisc, drop = FALSE)
+summary.D3dmirt <- function(object, ..., digits = 4){
+  tab1 <- as.data.frame(object$loadings, drop = FALSE)
+  tab2 <- as.data.frame(object$mdiff, drop = FALSE)
+  tab3 <- as.data.frame(object$mdisc, drop = FALSE)
   tab1 <- as.data.frame(cbind(tab1, tab2, tab3), drop = FALSE)
-  tab4 <- as.data.frame(cbind(x$dir.cos, x$degrees), drop = FALSE)
-  if (!is.null(x$c.dir.cos)){
-    tab5 <- as.data.frame(cbind(x$c.dir.cos, x$c.degrees), drop = FALSE)
-    c <- x$c
+  tab4 <- as.data.frame(cbind(object$dir.cos, object$degrees), drop = FALSE)
+  if (!is.null(object$c.dir.cos)){
+    tab5 <- as.data.frame(cbind(object$c.dir.cos, object$c.degrees), drop = FALSE)
+    c <- object$c
     items <- NULL
     for (i in seq_along(c)){
       l <- unlist(c[i])
@@ -36,12 +38,12 @@ summary.dmirt <- function(x){
       }
     }
   }
-  if (!is.null(x$c.dir.cos)){
-    sum <- list(dmirt.est = round(tab1,4), dmirt.angles = round(tab4, 4), construct.angles = round(tab5, 4))
+  if (!is.null(object$c.dir.cos)){
+    sum <- list(dmirt.est = round(tab1,digits), dmirt.angles = round(tab4, digits), construct.angles = round(tab5, digits))
   } else{
-    sum <- list(dmirt.est = round(tab1,4), dmirt.angles = round(tab4, 4))
+    sum <- list(dmirt.est = round(tab1,digits), dmirt.angles = round(tab4, digits))
   }
-  if (!is.null(x$c.dir.cos)){
+  if (!is.null(object$c.dir.cos)){
     cat(paste("\n3Dmirt object with", nrow(tab1), "items and", ncol(tab2), "levels of difficulty\n\n"))
     for (i in seq_along(c)){
       n <- c[i]
