2ad34d600faea963245aa68699cb2258d6544467
ForsbergPyschometrics <erik.forsberg@psychology.su.se>
2023-03-22 15:02:10 +0100

    Update modid.R

--- R/modid.R
+++ R/modid.R
@@ -9,22 +9,23 @@
 #'
 #' @return Lists of factors loadings and absolute sum scores (denoted with `modid`), sorted by the latter, and lastly a frame with raw factor loadings (denoted with `loadings`).
 #'
+#'
 #' @details Before performing descriptive item response theory analysis it is necessary to investigate if the item score patterns can be used to create empirically valid orthogonal axes in the model.
 #' For a three-dimensional model, this entails that at least two items must be selected.
 #' The first item should not show any systematic relation to the remaining two axes, and the second item should not be systematically related to the third axis.
 #' In praxis, this implies that item one, orthogonal to the y and z-axis, will identify the x-axis. Item two, orthogonal to the z-axis, will identify the y-axis, while the z-axis remains free.
 #'
-#'
+#' # Step 1: Explore Data Structure
 #' Thus, to begin the factor structure must be explored. This can be done using classical test theory exploratory factor methods (EFA), such as [psych::fa()] (Revelle, 2022). assuming three factors.
 #' Preferably, the EFA method should be carefully chosen based on theory or otherwise statistically reasonable.
 #'
-#'
+#' # Step 2: Item Selection
 #' Next, the `modid()` takes the factor structure assigned to a data frame, and outputs lists, one list per factor from the EFA.
 #' These lists contain one column for the loadings from each item on the factor of interest, and one column with absolute sum scores for each item calculated from the remaining factor loadings in the model.
 #' Each list is sorted with the lowest absolute sum score highest up.
 #' Accordingly, the top items are the items that best meet the assumption of orthogonality in the given item set or scale.
 #'
-#'
+#' Limitations
 #' Regarding item selection, model identification items should preferably have an absolute sum score of < .10 and factor loading > .80.
 #' If these conditions cannot be met, the user is advised to proceed with caution since the loading scores imply that an orthogonal structure may not be empirically attainable.
 #' However, it is also important to recognize that the `modid` function gives suggestions to the model identification and there could be situations where the researcher would need to use other methods.
@@ -56,7 +57,7 @@
 #' h <- data.frame(g$rotF)
 #' modid(h)
 #' }
-#' #' @export
+#' @export
 modid <- function(x, head = TRUE){
   if(!is.data.frame(x) && !is.matrix(x)) stop("Input object is not of type data frame or matrix")
   f <- NULL
