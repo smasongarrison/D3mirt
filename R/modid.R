0644941742423a06b82a717dbe5c1f2683964599
ForsbergPyschometrics <erik.forsberg@psychology.su.se>
2023-04-17 16:53:10 +0200

    Update modid.R

--- R/modid.R
+++ R/modid.R
@@ -1,6 +1,6 @@
 #' D3mirt Model Identification
 #'
-#' @description `modid()` performs model identification for within-dimensional modeling by indicating what items, from a set or scale, that maximize interpretation of the DMIRT model, such as the models given by `D3mirt()`.
+#' @description `modid()` performs model identification for descriptive multidimensional item response theory (DMIRT) models by indicating what items, from a set or scale, that maximize interpretation of the DMIRT model.
 #'
 #' @param x A data frame with factor loadings
 #' @param lower The lower bound for item inclusion based on item factor loadings. Default is `lower = 0.5`.
@@ -10,55 +10,64 @@
 #'
 #' @importFrom mirt mirt
 #'
-#' @details Before performing descriptive item response theory analysis it is necessary to identify the measurement model (Reckase, 2009), that will be used to specify the multidimensional graded response model with [`mirt::mirt`] (Chalmers, 2012; see [D3mirt::D3mirt] documentation for more details on model specification).
+#' @details Before performing DMIRT analysis it is necessary to identify the compensatory model (Reckase, 2009).
 #' For a three-dimensional model, this entails that two items must be chosen.
 #' If improper items are chosen the model will be hard to interpret in a meaningful way.
-#' The `modid` function was designed to maximize the former analytically by first order factors by sum of squares and then select the most optimal items.
-#' This help order the model so that the strongest loading items on the strongest factor always align with the x-axis.
+#' The `modid()` function was designed to maximize the former analytically.
+#' Briefly, the function first order factors by the sum of squares and from this select the strongest loading items that meet the statistical assumptions described above.
+#' This orders the model so that the strongest loading item, from the strongest factor, always aligns perfectly with the $x$-axis and that the other items follow thereon.
 #'
 #' Of the two items that must be chosen for `D3mirt`analysis, the first item is constrained not to load on the second and third axes (y and z),
 #' while the second item is only constrained not to load on the third axis (z).
-#' This will create an orthogonal structure with the first item locked in parallel on the x-axis.
+#' This will create an orthogonal structure with the first item fixed on the x-axis.
 #' The model identification process is briefly explained below.
 #'
 #' ## Step 1: Explore Data Structure
 #' To begin the factor structure must be explored with exploratory factor analysis (EFA).
-#' However, because `D3mirt` analysis is based on item response theory, it is recommended to use multidimensional item response theory EFA methods,
-#' such as the EFA option in [mirt::mirt] (Chalmers, 2012) with `ìtemtype = 'graded'`, so that the EFA is performed using the graded response model (Samejima, 1969) as the item model.
-#' This is highly beneficial because D3mirt analysis is based on the latter (see documentation in [D3mirt::D3mirt]).
-#'
-#' Regarding rotation method, EFA method and rotation should be carefully chosen based on theory or otherwise statistically reasonable.
-#' However, it is a good habit to test and compare several rotation options.
-#' Foremost, an EFA solution is inadequate if it cannot fit the orthogonal constraints described above.
+#' However, because `D3mirt` analysis is based on item response theory and the 2PL or graded response item model (Samejima, 1969), it is item response theory EFA methods,
+#' such as the EFA option in [mirt::mirt] (Chalmers, 2012) with `ìtemtype = 'graded'`, or `2PL` and number of factors set to 3.
 #'
 #' ## Step 2: Item Selection
-#' The `modid()` takes in the factor solution from the EFA, assigned to a data frame \emph{x}, and outputs lists (denoted \emph{items}) with suggestions of items to use for the model identification.
+#' The `modid()` takes in the factor solution from the EFA, assigned to a data frame \emph{x}, and outputs lists (denoted \emph{items}) with suggestions of items (\emph{item.1....item.n}) to use for the model identification.
 #' These lists contain one column for the loadings from each item on the factor of interest, and one column with absolute sum scores for each item calculated from the remaining factor loadings in the model.
 #' Each list is sorted with the lowest absolute sum score highest up.
 #' Accordingly, the top items in each list are the items that best meet the assumption of orthogonality in the analysis.
-#' Therefore, for a three-dimensional model, all else equal, the item highest up in the first list should be used to identify the x-axis, and the item highest up in the second list should be used to identify the y-axis, and so on
+#' Therefore, for a three-dimensional model, all else equal, the item highest up in the first list should be used to identify the x-axis, and the item highest up in the second list should be used to identify the y-axis, and so on.
 #'
 #' ## Criteria
-#' Optimized model identification items should preferably (a) have an absolute sum score of <= .10 and (b) with maximized factor loading on the factor of interest.
+#' Optimized model identification items should preferably (a) have an absolute sum score of less than or equal to .10 and (b) with maximized factor loading on the factor of interest.
 #' Of these two criteria, (a) should be given the strongest weight in the selection decision.
 #' If these conditions cannot be met, the user is advised to proceed with caution since the loading scores imply that an adequate orthogonal structure may not be empirically attainable.
-#' If problems occur, try change the rotation method for the EFA first hand.
-#' If this does not help, proceed by increasing the lower bound since this will allow the function to include weaker loading items in the item pool.
-#' The upper bound (set with argument `upper`) should not be increased > .1, unless the assumption of orthogonality can be compromised.
+#' If problems occur, try the following:
 #'
-#' The user also has the option of overriding the automatic sorting of factor order according to sum of squares loadings, with the argument `fac.order` (see examples section).
-#' This can, for instance, be useful in cases where the sum of sqaured loadings are very similar in strength in the EFA model.
+#' \enumerate{
+#' \item Change the rotation method in the EFA, e.g., to change from *oblimin* to *varimax*.
+#' \item Adjust the `lower` bound in `modid()`.
+#' \item Override factor order with `fac.order`.
+#' \item Adjust the `upper` bound in `modid()`.
+#' }
 #'
-#' The `modid()` function is not limited to three-dimensional analysis and can be used on any number of factors.
-#' Although based on suggestions on model identification given by Reckase (2009) for this type of analysis, the function offers some expansions that introduce more precision.
+#' The latter (point 4) should, however, only be used as a last resort.
+#' TThis is because the upper bound sets the upper limit for item inclusion.
+#' TAdjusting this limit too high means that the necessary statistical requirements are compromised.
+#' TThe lower limit (point 2), however, only increases the size of the item pool used for the item selection (see the section below on the model identification procedure).
+#' TIt is, therefore, recommended to adjust the lower limit up and down to see if the output differs, and from that make the final decision.
+#'
+#' The user also has the option of overriding the automatic sorting of factor order (point 4) with the argument `fac.order` (see examples section).
+#' This can be useful when there is only a very small difference between the squared factor loadings that in turn can
+#' causes problems (often only observable at later stages) when trying to find the best items for the model identification.
+#'
+#' Note, the `modid()` function is not limited to three-dimensional analysis and can be used on any number of factors.
+#' Moreover, although based on suggestions on model identification given by Reckase (2009) for this type of analysis, the function offers some expansions that introduce more precision.
 #' The latter foremost consist in incorporating sum of squares in the item selection process (unless the user has not specified otherwise).
 #' Experience tells that this is good practice that often leads to better results compared to other options.
-#' However, it is important to recognize that the model identification procedure only gives suggestions to the model specification, and there could be situations where the researcher should consider other methods.
+#' However, it is important to recognize that the `modid()` function only gives suggestions to the model specification, and there could be situations where the researcher should consider other methods.
 #'
 #' @return S3 object of class `modid` with lists of items and absolute sum scores, sorted by the latter, and sum of squared factor loadings and frame with raw factor loadings with columns ordered on explained variance (high to low) or according to user settings.
 #' @author Erik Forsberg
 #' @references Chalmers, R., P. (2012). mirt: A Multidimensional Item Response Theory Package for the R Environment. \emph{Journal of Statistical Software, 48}(6), 1-29.
 #' @references Reckase, M. D. (2009). \emph{Multidimensional Item Response Theory}. Springer.
+#' @references Reckase, M. D., & McKinley, R. L. (1991). The Discriminating Power of Items That Measure More Than One Dimension. \emph{Applied Psychological Measurement, 15}(4), 361-373–373. https://doi-org.ezp.sub.su.se/10.1177/014662169101500407
 #' @references Samejima, F. (1969). Estimation of latent ability using a response pattern of graded scores. \emph{Psychometrika 34}, 1–97. https://doi.org/10.1007/BF03372160
 #'
 #' @examples
